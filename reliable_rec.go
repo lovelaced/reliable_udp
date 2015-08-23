e3c2e0e57060c3a101bc25589ad098d66d4788f2
v <v@8chan.co>
2015-08-23 16:46:09 -0500

    Initial commit

---
+++ reliable_rec.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+	"strconv"
+)
+
+func check_packet(curr_packet int, last_recd int) int {
+	fmt.Println("curr_packet:", curr_packet, " last received:", last_recd)
+	if curr_packet == last_recd+1 {
+		ack(curr_packet)
+	} else {
+		return -1
+	}
+	return 0
+}
+
+func ack(curr_packet int) {
+	serv_addr,err := net.ResolveUDPAddr("udp","127.0.0.1:10002")
+	CheckError(err)
+
+	local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	CheckError(err)
+
+	conn, err := net.DialUDP("udp", local_addr, serv_addr)
+	CheckError(err)
+
+	defer conn.Close()
+
+	msg := strconv.Itoa(curr_packet)
+
+	buffer := []byte(msg)
+	fmt.Println("Sending ACK for", curr_packet)
+	_, err = conn.Write(buffer)
+	if err != nil {
+		fmt.Println(msg, err)
+	}
+	time.Sleep(time.Second * 1)
+}
---
+++ udp_server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+//	"bytes"
+	"strconv"
+)
+
+func CheckError(err error) {
+	if err  != nil {
+		fmt.Println("Error: " , err)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	serv_addr,err := net.ResolveUDPAddr("udp",":10001")
+	CheckError(err)
+
+	serv_conn,err := net.ListenUDP("udp", serv_addr)
+	CheckError(err)
+
+	defer serv_conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	var last_recd int = -1
+	for {
+		n,addr,err := serv_conn.ReadFromUDP(buffer)
+
+	//	binary_packet := bytes.NewBuffer(buffer[0:n])
+
+		var current_packet int
+		test, err := strconv.Atoi(string(buffer[0:n]))
+		current_packet = test
+
+	//	fmt.Println(binary_packet, current_packet)
+		fmt.Println("Received", current_packet, ", checking packet now.")
+		chk := check_packet(current_packet, last_recd)
+		if chk == -1 {
+			continue
+		}
+		fmt.Println("Successfully received", string(buffer[0:n]), "from", addr)
+		last_recd, err = strconv.Atoi(string(buffer[0:n]))
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
+}
+
