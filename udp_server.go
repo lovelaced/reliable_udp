bd498eafbaba7590b52e2b9eff909d2f972454bb
v <v@8chan.co>
2015-08-24 19:02:00 -0500

    cleaned up a little

--- udp_server.go
+++ udp_server.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
-//	"bytes"
 	"strconv"
 )
+// keep track of the most recent packet we've accepted
+var most_recent int = -1
 
 func CheckError(err error) {
 	if err  != nil {
@@ -14,7 +15,7 @@ func CheckError(err error) {
 		os.Exit(0)
 	}
 }
-
+//initialize incoming connections
 func main() {
 	serv_addr, err := net.ResolveUDPAddr("udp", ":10001")
 	CheckError(err)
@@ -23,31 +24,33 @@ func main() {
 	CheckError(err)
 
 	defer serv_conn.Close()
+	// go read from the connection
 	read(serv_conn)
 }
+
 func read(serv_conn *net.UDPConn) {
 
 	buffer := make([]byte, 1024)
-
+	// number of last received packet
 	var last_recd int = 0
 	for {
-		fmt.Println("Reading from UDP buffer...")
+//		fmt.Println("Reading from UDP buffer...")
 		n,addr,err := serv_conn.ReadFromUDP(buffer)
 
-	//	binary_packet := bytes.NewBuffer(buffer[0:n])
-
-		var current_packet int
-		test, err := strconv.Atoi(string(buffer[0:n]))
-		current_packet = test
-
-	//	fmt.Println(binary_packet, current_packet)
+		// get the current packet number
+		current_packet, err := strconv.Atoi(string(buffer[0:n]))
+		CheckError(err)
+		// check the packet we have now against the one we received last
 		go check_packet(serv_conn, current_packet, last_recd)
-
-		fmt.Println("Successfully received", string(buffer[0:n]), "from", addr)
 		last_recd, err = strconv.Atoi(string(buffer[0:n]))
-		if err != nil {
-			fmt.Println("Error: ", err)
+		// check for duplicates and discard them as necessary, otherwise mark as received
+		if last_recd == most_recent {
+			fmt.Println("Duplicate packet found, discarding", string(buffer[0:n]))
+		} else {
+			fmt.Println("Successfully received", string(buffer[0:n]), "from", addr)
+			most_recent = last_recd
 		}
+		CheckError(err)
 	}
 }
 
